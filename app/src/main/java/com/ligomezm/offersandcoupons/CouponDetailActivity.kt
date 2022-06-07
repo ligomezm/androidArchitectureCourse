607f4eedabc5b792813220eb7a820e93f30aa50e
Liliana Gomez <gomez.liliana333@gmail.com>
2022-06-07 11:32:29 -0400

    First commit

---
+++ app/src/main/java/com/ligomezm/offersandcoupons/CouponDetailActivity.kt
@@ -0,0 +1,20 @@
+package com.ligomezm.offersandcoupons
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+
+class CouponDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coupon_detail)
+
+    }
+}
